1d68ec4832ad63283f550e7999b5cda26db6ab71
ttdp <tiantong.dev@gmail.com>
2020-02-12 17:14:23 +0800

    Add Fluent

--- Package.swift
+++ Package.swift
@@ -8,11 +8,13 @@ let package = Package(
     ],
     dependencies: [
         // 💧 A server-side Swift web framework.
-        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0-beta")
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0-beta"),
+        .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0-beta"),
+        .package(url: "https://github.com/vapor/fluent-sqlite-driver.git", from: "4.0.0-beta")
     ],
     targets: [
         .target(name: "App", dependencies: [
-            "Vapor"
+            "Vapor", "Fluent", "FluentSQLiteDriver", 
         ]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App", "XCTVapor"])
